1c1fd9e65b3c51763c11eb84b203a42e580cbf82
goodluck218 <goodluckmaduabuchi92@gmail.com>
2024-09-10 21:27:46 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,25 @@
+// CREATOR : ABROTECH
+//NOTE: how to become premium please go to file; all/database/premium.json
+//NOTE: how to become a public owner, please go to file; all/database/owner.json
+// CREDIT : DELTA,HW MODS
+require("./all/module")
+global.owner = "2348100151048" 
+global.namaCreator = "ABRO TECH" 
+global.autoJoin = false 
+global.antilink = false 
+global.versisc = '1.0' 
+global.codeInvite = ""
+global.isLink = ' ' 
+global.thumb = fs.readFileSync("./thumb.png") 
+global.audionya = fs.readFileSync("./all/sound.mp3") 
+global.packname = "BOT" 
+global.author = "ABRO TECH" 
+global.jumlah = "5" 
+global.namabot = "LADY BUG 5.0" 
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update ${__filename}`))
+	delete require.cache[file]
+	require(file)
+})
